21dd9f377ef38252a114830520f3ab22d8253925
Ukang'a Dickson <ukanga@shiver.(none)>
2010-03-27 21:31:59 +0300

    migrate code for muac

--- apps/childcount/forms/MigrationForm.py
+++ apps/childcount/forms/MigrationForm.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from childcount.forms import CCForm
 from childcount.models import Encounter
+from childcount.models.reports import NutritionReport
 
 
 class MigrationForm(CCForm):
@@ -21,3 +22,15 @@ class MigrationForm(CCForm):
                 created_on = datetime.strptime(created_on,"%Y-%m-%d %H:%M:%S")
                 patient.created_on = created_on
                 patient.save()
+        if self.params[1].lower() == "muac":
+            #pick status created_at
+            if len(self.params[2:]) >= 2:
+                created_on = self.params[3].upper().replace('T', ' ')
+                created_on = datetime.strptime(created_on,"%Y-%m-%d %H:%M:%S")
+                status = int(self.params[2])
+                nr = NutritionReport.objects.filter(encounter=self.encounter)[0];
+                nr.status = status
+                nr.save()
+                dir(self.encounter)
+                self.encounter.encounter_date = created_on
+                self.encounter.save()
