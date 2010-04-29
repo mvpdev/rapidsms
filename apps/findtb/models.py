486f6eed0d81d0967d6b948c60d287b2b02d1bb7
David Gelvin <david.gelvin@gmail.com>
2010-04-29 19:00:50 +0300

    Added config model and fixture

--- apps/findtb/models.py
+++ apps/findtb/models.py
@@ -5,6 +5,7 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import Group, User
+from django.db.models.signals import post_delete, pre_save
 
 from locations.models import Location
 from reporters.models import Reporter
@@ -15,16 +16,33 @@ class Role(models.Model):
     reporter = models.ForeignKey(Reporter)
     location = models.ForeignKey(Location, blank=True, null=True)
 
-    def save(self):
-        if self.reporter.user_ptr not in self.group.user_set.all():
-            self.reporter.groups.add(self.group)
-        super(Role, self).save() # Call the "real" save() method
+    def __unicode__(self):
+        return "%(reporter)s as %(group)s at %(location)s" % \
+               {'reporter':self.reporter, 'group':self.group, \
+                'location':self.location}
 
-    def delete(self):
-        if Role.objects.filter(reporter=self.reporter, \
-                               group=self.group).count() == 1:
-            self.reporter.groups.remove(self.group)
-        super(Role, self).delete() # Call the "real" delete() method
+def Role_delete_handler(sender, **kwargs):
+    role = kwargs['instance']
+    if not Role.objects.filter(reporter=role.reporter, \
+                               group=role.group).count():
+        role.reporter.groups.remove(role.group)
+post_delete.connect(Role_delete_handler, sender=Role)
+
+def Role_presave_handler(sender, **kwargs):
+    role = kwargs['instance']
+    role.reporter.groups.add(role.group)
+    if role.pk:
+        orig_role = Role.objects.get(pk=role.pk)
+        if orig_role.reporter != role.reporter:
+            if Role.objects.filter(reporter=orig_role.reporter, \
+                                   group=orig_role.group).count() == 1:
+                orig_role.reporter.groups.remove(orig_role.group)
+        else:
+            if orig_role.group != role.group:
+                if Role.objects.filter(reporter=role.reporter, \
+                                       group=orig_role.group).count() == 1:
+                    role.reporter.groups.remove(orig_role.group)
+pre_save.connect(Role_presave_handler, sender=Role)
 
 
 class Patient(models.Model):
@@ -77,7 +95,7 @@ class Sputum(models.Model):
 
 class FINDTBGroup(Group):
     CLINICIAN_GROUP_NAME = 'clinician'
-    DTU_LAB_TECH_GROUP_NAME = 'dtu lab technician'
+    DTU_LAB_TECH_GROUP_NAME = 'dtu lab tech'
     DISTRICT_TB_SUPERVISOR_GROUP_NAME = 'district tb supervisor'
     ZONAL_TB_SUPERVISOR_GROUP_NAME = 'zonal tb supervisor'
 
@@ -95,3 +113,42 @@ class FINDTBGroup(Group):
 
     def isZTLS(self):
         return self.name == self.ZONAL_TB_SUPERVISOR
+
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: ukanga
+
+'''Config Model for child count
+
+'''
+
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Configuration(models.Model):
+
+    '''Store Key/value config options'''
+
+    description = models.CharField(_('Description'), max_length=255, \
+                                   db_index=True)
+    key = models.CharField(_('Key'), max_length=50, db_index=True)
+    value = models.CharField(_('Value'), max_length=255, \
+                             db_index=True, blank=True)
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.key, self.value)
+
+    def get_dictionary(self):
+        return {'key': self.key, 'value': self.value, \
+                'description': self.description}
+
+    @classmethod
+    def has_key(cls, key):
+        return cls.objects.filter(key=key).count() != 0
+
+    @classmethod
+    def get(cls, key):
+        '''get config value of specified key'''
+        cfg = cls.objects.get(key__iexact=key)
+        return cfg.value
