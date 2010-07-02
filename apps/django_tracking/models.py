9a7d9f3b9b76dae4a2206cd8064320822912ca05
ksamuel <info.ksamuel@gmail.com>
2010-07-02 12:33:43 +0200

    models.py

--- apps/django_tracking/models.py
+++ apps/django_tracking/models.py
@@ -79,11 +79,11 @@ class State(models.Model):
     # allow filtering state by a type, whatever it means
     type = models.CharField(max_length=30, blank=True)
 
-    # allow filtering state by origin, whatever it means
-    origin = models.CharField(max_length=30, blank=True)
-
     # allow filtering state by a title, whatever it means
     title = models.CharField(max_length=60, blank=True)
+
+    # allow filtering state by origin, whatever it means
+    origin = models.CharField(max_length=30, blank=True)
 
     # a cancelled state don't appear in the history, but is not distroyed
     cancelled = models.BooleanField(default=False)
