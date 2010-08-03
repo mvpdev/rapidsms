604e42c944cdda866b3075084ccccf3e8c8dd7df
ksamuel <info.ksamuel@gmail.com>
2010-08-03 11:45:23 +0300

    Added callback to the message sending

--- apps/direct_sms/app.py
+++ apps/direct_sms/app.py
@@ -3,6 +3,17 @@
 
 import rapidsms
 from reporters.models import Reporter
+from logger.models import OutgoingMessage
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+    
+def return_none(**kwargs): pass
+
+PICKLED_LAMBDA = pickle.dumps( return_none)
+PICKLED_DICT = pickle.dumps({})
 
 class App (rapidsms.app.App):
     """
@@ -14,19 +25,34 @@ class App (rapidsms.app.App):
 
 
     def ajax_POST_send_message(self, urlparser, post):
-       """
-       Callback method for sending messages from the webui via the ajax app.
-       """
-       rep = Reporter.objects.get(pk=post["reporter"])
-       pconn = rep.connection()
+        """
+        Callback method for sending messages from the webui via the ajax app.
+        """
+        
+        
+        rep = Reporter.objects.get(pk=post["reporter"])
+        pconn = rep.connection()
 
-       # abort if we don't know where to send the message to
-       # (if the device the reporter registed with has been
-       # taken by someone else, or was created in the WebUI)
-       if pconn is None:
+        callback = pickle.loads(post.get("callback", 
+                                            PICKLED_LAMBDA)) or return_none
+        callback_kwargs = pickle.loads(post.get("callback_kwargs", 
+                                        PICKLED_DICT)) or {}
+
+        # abort if we don't know where to send the message to
+        # (if the device the reporter registed with has been
+        # taken by someone else, or was created in the WebUI)
+        if pconn is None:
            raise Exception("%s is unreachable (no connection)" % rep)
 
-       # attempt to send the message
-       # TODO: what could go wrong here?
-       be = self.router.get_backend(pconn.backend.slug)
-       return be.message(pconn.identity, post["text"]).send()
+        # attempt to send the message
+        # TODO: what could go wrong here?
+        be = self.router.get_backend(pconn.backend.slug)
+        message = be.message(pconn.identity, post["text"])
+        sent = message.send()
+
+        # attempt to call the callback to get the message back
+        callback(outgoing_message=OutgoingMessage.objects.get(pk=message.logger_id),
+                  **callback_kwargs)
+        
+        return sent
+        
--- apps/direct_sms/utils.py
+++ apps/direct_sms/utils.py
@@ -5,24 +5,67 @@
 Helpers to send sms without caring about AJAX
 """
 
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
 import urllib2
 from urllib import urlencode
 
 from django.conf import settings
 
-def send_msg(reporter, text):
-   '''
+
+def store_log(outgoing_message, model, field='message', 
+              reload_model=False, save=True):
+    """
+        Store the message reference into into of field of the given model.
+        
+        Default field is 'message'. If reload_model is True, reload a fresh
+        model from the DB before processing. If save is True (default),
+        the model will be saved.
+    """
+    if reload_model:
+        model = model.__class__.objects.get(pk=r.pk)
+
+    model.__dict__[field] = outgoing_message
+
+    if save:
+        model.save()
+    
+    
+
+def send_msg(reporter, text, 
+            callback=None, callback_kwargs=None, 
+            log_in_model=None):
+    '''
     Sends a message to a reporter using the ajax app. This goes to
-    ajax_POST_send_message in the app.py
+    ajax_POST_send_message in the app.py.
+
+    If you set a call back, it will be called at the message sending with
+    the message as first argument, and callback_args as misc kwargs.
+
+    Default is to use "store_log" if a model is passed.
+    
+    The callback function is pickled, and therfore can not be a lambda and 
+    must be defined at the module level.
+    It must accept **kwargs.
     '''
 
-   conf = settings.RAPIDSMS_APPS['ajax']
-   url = "http://%s:%s/direct-sms/send_message" % (conf["host"], conf["port"])
+    if log_in_model:
+        callback = store_log
+        callback_kwargs = {'model': log_in_model}
 
-   data = {'reporter': reporter.pk, \
-           'text': text}
-   req = urllib2.Request(url, urlencode(data))
-   stream = urllib2.urlopen(req)
-   stream.close()
+    conf = settings.RAPIDSMS_APPS['ajax']
+    url = "http://%s:%s/direct-sms/send_message" % (conf["host"], conf["port"])
+
+    data = {'reporter': reporter.pk, 
+           'text': text,
+           'callback': pickle.dumps(callback),
+           'callback_kwargs': pickle.dumps(callback_kwargs)}
+           
+    req = urllib2.Request(url, urlencode(data))
+    stream = urllib2.urlopen(req)
+    stream.close()
 
 
