4d46d5e794058894631fd7b033bb54c112c0b6b8
ksamuel <info.ksamuel@gmail.com>
2010-08-12 18:40:11 +0200

    Removing codes from logger_ng and making it use direct_sms

--- apps/direct_sms/utils.py
+++ apps/direct_sms/utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
 
 """
 Helpers to send sms without caring about AJAX
@@ -15,29 +16,83 @@ from urllib import urlencode
 
 from django.conf import settings
 
+from exceptions import DirectSmsError
 
-def store_log(outgoing_message, model, field='message', 
-              reload_model=False, save=True):
-    """
-        Store the message reference into into of field of the given model.
+NO_LOGGER = True
+
+# try to store log using the usual logger
+try:
+    from logger.models import OutgoingMessage
+except ImportError:
+    pass
+else:
+    NO_LOGGER = False
+    def store_log(outgoing_message, model, field='message', 
+                  reload_model=False, save=True):
+        """
+            Store the message reference into into of field of the given model.
+            
+            Default field is 'message'. If reload_model is True, reload a fresh
+            model from the DB before processing. If save is True (default),
+            the model will be saved.
+        """
         
-        Default field is 'message'. If reload_model is True, reload a fresh
-        model from the DB before processing. If save is True (default),
-        the model will be saved.
-    """
-    if reload_model:
-        model = model.__class__.objects.get(pk=r.pk)
+        outoing_message = OutgoingMessage.objects\
+                                         .get(pk=outgoing_message.logger_id)
+        
+        if reload_model:
+            model = model.__class__.objects.get(pk=r.pk)
 
-    model.__dict__[field] = outgoing_message
+        model.__dict__[field] = outgoing_message
 
-    if save:
-        model.save()
+        if save:
+            model.save()
     
     
+# try to store log using the new logger  
+try:
+    from logger_ng.models import LoggedMessage
+except ImportError:
+    pass
+else:
+    NO_LOGGER = False
+    def store_log(outgoing_message, model, field='message', 
+                  reload_model=False, save=True):
+        """
+            Store the message reference into into of field of the given model,
+            but using the new 
+            
+            Default field is 'message'. If reload_model is True, reload a fresh
+            model from the DB before processing. If save is True (default),
+            the model will be saved.
+        """
+        
+        outoing_message = LoggedMessage.objects\
+                                       .get(pk=outgoing_message.logger_id)
+        
+        if reload_model:
+            model = model.__class__.objects.get(pk=r.pk)
 
-def send_msg(reporter, text, 
+        model.__dict__[field] = outgoing_message
+
+        if save:
+            model.save() 
+    
+    
+# no logger present, this shorcut can't work
+if NO_LOGGER:
+    def store_log(outgoing_message, model, field='message', 
+                  reload_model=False, save=True):
+        """
+            Prevent this fonction to be used.
+        """
+        raise DirectSmsError(u"No compatible logger application is present")
+        
+
+
+def send_msg(reporter=None, text='', 
             callback=None, callback_kwargs=None, 
-            log_in_model=None):
+            log_in_model=None, backend='', identity=''):
     '''
     Sends a message to a reporter using the ajax app. This goes to
     ajax_POST_send_message in the app.py.
@@ -58,14 +113,22 @@ def send_msg(reporter, text,
 
     conf = settings.RAPIDSMS_APPS['ajax']
     url = "http://%s:%s/direct-sms/send_message" % (conf["host"], conf["port"])
+    
+    data = {'text': text,
+            'callback': pickle.dumps(callback),
+            'callback_kwargs': pickle.dumps(callback_kwargs)}
 
-    data = {'reporter': reporter.pk, 
-           'text': text,
-           'callback': pickle.dumps(callback),
-           'callback_kwargs': pickle.dumps(callback_kwargs)}
+    try:
+        data['reporter'] = reporter.pk
+    except AttributeError:
+        pass
+        
+    data['backend'] = backend
+    data['identity'] = identity
            
     req = urllib2.Request(url, urlencode(data))
     stream = urllib2.urlopen(req)
     stream.close()
+
 
 
--- apps/logger_ng/utils.py
+++ apps/logger_ng/utils.py
@@ -12,25 +12,27 @@ from urllib import urlencode
 import rapidsms
 from rapidsms.webui import settings
 
-from logger_ng import config
+import config
+from models import LoggedMessage
+
+from direct_sms.utils import send_msg
+
+
+def watermak(outgoing_message, in_response_to):
+    """
+        Inject the in the outgoing message id of the message we want to respond 
+        to.
+    """
+    outgoing_message.logger_id = in_response_to
+    outgoing_message.status = LoggedMessage.STATUS_LOGGER_RESPONSE
 
 
 def respond_to_msg(msg, text):
     '''
     Sends a message to a reporter using the ajax app.  This goes to
-    ajax_POST_send_message in logger_ng app.py.
-
-    It is passed a LoggedMessage object (to which we are responding) and the
-    text of our response.
-
-    This is dependent on the rapidsms ajax app.
+    ajax_POST_send_message in direct_sms app.py and use a callback to 
+    watermark the response.
     '''
-    conf = settings.RAPIDSMS_APPS['ajax']
-    slug = config.title.replace(' ', '-').lower()
-    url = "http://%s:%s/%s/send_message" % (conf["host"], conf["port"], slug)
-
-    data = {'msg_pk': msg.pk, \
-            'text': text}
-    req = urllib2.Request(url, urlencode(data))
-    stream = urllib2.urlopen(req)
-    stream.close()
+    send_msg(backend=msg.backend, text=text, identity=msg.identity,
+             callback=watermak, callback_kwargs={'in_response_to': msg.id})
+             
