009fd11589dce5344084eb8a629d1725294814fd
David Gelvin <david.gelvin@gmail.com>
2010-09-03 08:00:49 +0000

    Added hack to fix tab permissions

---
+++ apps/notice/models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# maintainer: dgelvin
+
+from django.db import models
+
+class Hack(models.Model):
+    """ Hack to get can_view permission """
+
+    class Meta:
+        app_label = 'notice'
+        permissions = (
+            ("can_view", "Can view"),
+        )
