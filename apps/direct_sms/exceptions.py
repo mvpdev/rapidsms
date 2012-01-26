0c23e78229124b37105d798d9adbe6eccce483b0
Ukang'a Dickson <ukanga@gmail.com>
2012-01-27 00:13:36 +0300

    correct vim encoding setting

--- apps/direct_sms/exceptions.py
+++ apps/direct_sms/exceptions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 
 class DirectSmsError(Exception):
--- apps/direct_sms/utils.py
+++ apps/direct_sms/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 """
 Helpers to send sms without caring about AJAX
