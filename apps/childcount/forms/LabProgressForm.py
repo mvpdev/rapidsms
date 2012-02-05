3a40b72ce4d84f5e61500452ea209788fbb628f2
katembu <mose.kate@gmail.com>
2012-02-05 21:02:30 +0300

    update on Lab form response

--- apps/childcount/forms/LabProgressForm.py
+++ apps/childcount/forms/LabProgressForm.py
@@ -18,8 +18,12 @@ from childcount.exceptions import ParseError
 from childcount.forms.utils import MultipleChoiceField
 
 
+from alerts.utils import SmsAlert
+
+
 class LabProgressForm(CCForm):
-    """ LAB Rquisition Progress Form
+    """
+    LAB Requisition Progress Form
 
     """
 
@@ -55,7 +59,7 @@ class LabProgressForm(CCForm):
             raise ParseError(_(u"Not enough info.Expected: LAB Results "\
                                 "Requested"))
 
-        ''' 
+        '''
         GET ANY OPEN, LAB REPORT
         USE SAMPLE NUMBER AS IDENTIFIER
         '''
@@ -65,32 +69,77 @@ class LabProgressForm(CCForm):
             labtest = LabReport.objects.get(sample_no=sample_no)
         except LabReport.DoesNotExist:
             raise ParseError(_(u"Unknown LabTest Request (%s) Check " \
-                                "Sampleno and try again")  %sample_no )
-
+                                "Sampleno and try again") % sample_no)
 
         progress_field.set_language(self.chw.language)
 
         #Get Progress STATUS
         req_code = self.params[2]
         if not progress_field.is_valid_choice(req_code):
-            raise ParseError(_(u"| Req. Progress code should be %(choices)s.") \
+            raise ParseError(_(u"Req. Progress code should be %(choices)s.") \
                              % {'choices': progress_field.choices_string()})
 
         labtest.progress_status = progress_field.get_db_value(req_code)
 
         if labtest.progress_status == LabReport.PRO_RECEIVED:
             labtest.status = LabReport.STATUS_INPROGRESS
-        if labtest.progress_status  == LabReport.PRO_NOSAMPLE:
+            msg = _(u"The sample for %(hh)s, %(sn)s %(test)s has been " \
+                     "received.") % {'hh': \
+                                        labtest.encounter.patient.health_id, \
+                                     'sn': labtest.sample_no, \
+                                     'test': labtest.lab_test.name,
+                                    }
+
+        if labtest.progress_status == LabReport.PRO_NOSAMPLE:
             labtest.status = LabReport.STATUS_STALLED
-        if labtest.progress_status  == LabReport.PRO_INSUFFICIENT:
+            msg = _(u"The lab did not receive a sample for %(hh)s, %(sn)s " \
+                     "%(test)s, please contact the lab to resolve this" \
+                     " issue. ") % {'hh': \
+                                        labtest.encounter.patient.health_id, \
+                                     'sn': labtest.sample_no, \
+                                     'test': labtest.lab_test.name,
+                                    }
+
+        if labtest.progress_status == LabReport.PRO_INSUFFICIENT:
             labtest.status = LabReport.STATUS_STALLED
-        if labtest.progress_status  == LabReport.PRO_WRONGTYPE:
+            msg = _(u"There is not enough of the sample for %(hh)s, " \
+                     "%(sn)s %(test)s collected to perform the test, " \
+                     "please recall the patient and collect a new sample of " \
+                     "sufficient quantity ") % {'hh': \
+                                     labtest.encounter.patient.health_id, \
+                                     'sn': labtest.sample_no, \
+                                     'test': labtest.lab_test.name,
+                                    }
+
+        if labtest.progress_status == LabReport.PRO_WRONGTYPE:
             labtest.status = LabReport.STATUS_STALLED
-        if labtest.progress_status  == LabReport.PRO_BADQUALITY:
+            msg = _(u"The sample sent for %(hh)s, req %(sn)s %(test)s was " \
+                     "of the wrong type for the test, please recall the " \
+                     "patient and collect a new sample, if you need advice " \
+                     "contact the lab ") % {'hh': \
+                                     labtest.encounter.patient.health_id, \
+                                     'sn': labtest.sample_no, \
+                                     'test': labtest.lab_test.name,
+                                    }
+
+        if labtest.progress_status == LabReport.PRO_BADQUALITY:
             labtest.status = LabReport.STATUS_STALLED
+            msg = _(u"The quality of the sample for %(hh)s, req %(sn)s " \
+                     "%(test)s is bad, please recall the patient and collect" \
+                     "a new sample, if you need advice contact the lab.") \
+                      % {'hh': labtest.encounter.patient.health_id, \
+                         'sn': labtest.sample_no, \
+                         'test': labtest.lab_test.name,
+                        }
 
         labtest.save()
-
         self.response = _(u"Requisition Progress: %(req_code)s") % \
                           {'req_code': labtest.get_progress_status_display()}
 
+        #Alert originator of the Message
+        if labtest.encounter.chw:
+            r = labtest.encounter.chw.reporter
+            alert = SmsAlert(reporter=r, msg=msg)
+            sms_alert = alert.send()
+            sms_alert.name = "Lab Results"
+            sms_alert.save()
