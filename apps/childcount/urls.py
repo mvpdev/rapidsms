f7f0a56651a42f109ae1f24b401477eb767d8c5c
Ukang'a Dickson <ukanga@gmail.com>
2012-04-04 12:35:06 +0300

    load dashboard elements asynchronously

--- apps/childcount/urls.py
+++ apps/childcount/urls.py
@@ -25,12 +25,12 @@ urlpatterns = patterns('',
     admin_urls,
 
     # webapp URLS
-    url(r'^accounts/login/$', "webapp.views.login", \
-                     {'template_name': 'childcount/login.html'}, name='login'),
-    url(r'^accounts/logout/$', "webapp.views.logout", \
-                {'template_name': 'childcount/loggedout.html'}, name='logout'),
+    url(r'^accounts/login/$', "webapp.views.login",
+        {'template_name': 'childcount/login.html'}, name='login'),
+    url(r'^accounts/logout/$', "webapp.views.logout",
+        {'template_name': 'childcount/loggedout.html'}, name='logout'),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url('^static/webapp/(?P<path>.*)$', 'django.views.static.serve', \
+    url('^static/webapp/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': '%s/static' % os.path.dirname(webapp.__file__)}),
 
     url(r'^$', views.index, name='dashboard'),
@@ -38,28 +38,32 @@ urlpatterns = patterns('',
     url(r'^childcount/patients/?$', views.patient, name='cc-patients'),
     url(r'^childcount/patients/edit/((?P<healthid>[a-zA-Z0-9]+)/)?$',
         views.edit_patient, name='cc-edit_patient'),
-    url(r'^childcount/patients/(?P<chw>[a-zA-Z0-9]+)/?$', \
+    url(r'^childcount/patients/(?P<chw>[a-zA-Z0-9]+)/?$',
         views.patient, name='cc-patients-chw'),
     url(r'^childcount/patients/(?P<page>\d+)/?$', views.patient),
 
     url(r'^childcount/chws.json/?$', views.chw_json),
     url(r'^childcount/add_chw/?$', views.add_chw, name='cc-add_chw'),
     url(r'^childcount/list_chw/?$', views.list_chw, name='cc-list_chw'),
-    url(r'^childcount/change_chw/(?P<chw>[a-zA-Z0-9\-\_\.]*)/?$', \
+    url(r'^childcount/change_chw/(?P<chw>[a-zA-Z0-9\-\_\.]*)/?$',
         views.change_chw, name='cc-change_chw'),
-    url(r'^childcount/list_location/?$', views.list_location, \
+    url(r'^childcount/list_location/?$', views.list_location,
         name='cc-list_location'),
-    url(r'^childcount/change_location/(?P<location>\d+)/?$', \
+    url(r'^childcount/change_location/(?P<location>\d+)/?$',
         views.change_chw_by_location, name='cc-change_location'),
-    url(r'^childcount/patient-by-location/(?P<location>\d+)/?$', \
+    url(r'^childcount/patient-by-location/(?P<location>\d+)/?$',
         views.patient_by_location, name='cc-patients-by-location'),
     url(r'^childcount/indicators/?$', views.indicators, name='cc-indicators'),
 
     url(r'^childcount/dataentry/?$', views.dataentry, name='cc-dataentry'),
-    url(r'^childcount/dataentry/form/(?P<formid>[a-zA-Z0-9\-\_\.]*)/?$', \
+    url(r'^childcount/dataentry/form/(?P<formid>[a-zA-Z0-9\-\_\.]*)/?$',
                         views.form, name='form'),
-    url(r'^childcount/site_summary/(?P<report>[a-z_]*)/(?P<format>[a-z]*)$', \
+    url(r'^childcount/site_summary/(?P<report>[a-z_]*)/(?P<format>[a-z]*)$',
         views.site_summary),
 
-    url(r'^childcount/upload-healthid-file', uploadhealthids.upload_file, name='cc-upload-hids'),
+    url(r'^childcount/upload-healthid-file',
+        uploadhealthids.upload_file, name='cc-upload-hids'),
+
+    url(r'^childcount/dashboard-section/(?P<section_name>[a-z_]*)/$',
+        views.get_dashboard_section, name='cc-dashboard-section'),
 )
