b49cace4f0060e1abb7aca2e7970b98ac44e3a1e
Ukang'a Dickson <ukanga@gmail.com>
2012-05-10 13:48:00 +0300

    comment out sql statement

--- tools/match_omrs_users.py
+++ tools/match_omrs_users.py
@@ -47,9 +47,9 @@ revision.start()
 ### INTO OUTFILE '/tmp/users.csv' 
 ### FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 ###   LINES TERMINATED BY '\n'
-###
+### FROM users;
 
-FROM users;
+
 with open('users.csv') as f:
     unknown = '';
     for line in f:
