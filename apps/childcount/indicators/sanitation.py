c8dbad786621fb8d14e74be4ea1bf1e578298b39
katembu <mose.kate@gmail.com>
2012-06-05 14:30:29 +0300

    Sanitation indicator

--- apps/childcount/indicators/sanitation.py
+++ apps/childcount/indicators/sanitation.py
@@ -14,6 +14,14 @@ from childcount.models.reports import SanitationReport
 
 NAME = _("Sanitation")
 
+improved = [SanitationReport.FLUSH,
+            SanitationReport.PITLAT_WITH_SLAB,
+            SanitationReport.VENTILATED_IMPROVED_PIT,
+            SanitationReport.COMPOSTING_TOILET]
+
+share = SanitationReport.SHARE_NO
+
+
 class Total(Indicator):
     type_in     = QuerySetType(Patient)
     type_out    = int
@@ -31,3 +39,63 @@ class Total(Indicator):
                     (period.start, period.end))\
             .count()
 
+class UniqueHousehold(Indicator):
+    type_in     = QuerySetType(Patient)
+    type_out    = int
+
+    slug        = "uniqueSANReports"
+    short_name  = _("Unique +SAN Reports")
+    long_name   = _("Total number of Unique sanitation reports")
+
+    @classmethod
+    def _value(cls, period, data_in):
+        return SanitationReport\
+            .objects\
+            .filter(encounter__patient__in=data_in,\
+                encounter__encounter_date__range=\
+                    (period.start, period.end))\
+            .values('encounter__patient')\
+            .distinct()\
+            .count()
+
+
+class UsingImprovedSanitation(Indicator):
+    type_in     = QuerySetType(Patient)
+    type_out    = int
+
+    slug        = "improvedSanitation"
+    short_name  = _("Using Improved Sanitation")
+    long_name   = _("Total number of Unique household Using Improved Sanitation")
+
+    @classmethod
+    def _value(cls, period, data_in):
+        return SanitationReport\
+            .objects\
+            .filter(encounter__patient__in=data_in,\
+                encounter__encounter_date__range=\
+                    (period.start, period.end),\
+                toilet_lat__in=improved)\
+            .latest_for_patient().distinct()\
+            .count()
+
+
+class ImprovedSanitationDontshare(Indicator):
+    type_in     = QuerySetType(Patient)
+    type_out    = int
+
+    slug        = "improvedSanitationdontshare"
+    short_name  = _("Using Improved Sanitation dont share")
+    long_name   = _("Total number of Unique household Using Improved " \
+                    "Sanitation and dont share")
+
+    @classmethod
+    def _value(cls, period, data_in):
+        return SanitationReport\
+            .objects\
+            .filter(encounter__patient__in=data_in,\
+                encounter__encounter_date__range=\
+                    (period.start, period.end),\
+                toilet_lat__in=improved, \
+                share_toilet=share)\
+            .latest_for_patient().distinct()\
+            .count()
